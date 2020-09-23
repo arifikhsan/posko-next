c47456114bffef034a28ad9baaae38dc50e0790b
Arif Ikhsanudin <arif.ikhsanudin.developer@gmail.com>
2020-09-23 11:25:16 +0700

    save

--- pages/index.js
+++ pages/index.js
@@ -18,7 +18,7 @@ export default function Home() {
               <section className="text-gray-700 body-font">
                 <div className="container px-5 py-8 mx-auto">
                   <div className="flex flex-wrap -m-4">
-                    <div className="p-4 md:w-1/2 lg:w-1/3">
+                    <div className="w-full p-4 md:w-1/2 lg:w-1/4">
                       <div className="flex flex-col h-full p-8 bg-gray-100 rounded-lg">
                         <div className="flex items-center mb-3">
                           <h2 className="text-2xl font-medium text-gray-900 title-font">
@@ -26,12 +26,12 @@ export default function Home() {
                           </h2>
                         </div>
                         <div className="flex-grow mt-2">
-                          <p className="inline-flex items-baseline">
-                            <span className="text-xl font-semibold">27</span>
+                          <p className="inline-flex items-end">
+                            <span className="text-2xl font-semibold">27</span>
                             <span className="ml-2">Posko</span>
                           </p>
-                          <p className="inline-flex items-baseline mt-2">
-                            <span className="text-xl font-semibold text-red-500">
+                          <p className="inline-flex items-end mt-2">
+                            <span className="text-2xl font-semibold text-red-500">
                               20%
                             </span>
                             <span className="ml-2">Bantuan terpenuhi</span>
@@ -39,7 +39,7 @@ export default function Home() {
                         </div>
                       </div>
                     </div>
-                    <div className="p-4 md:w-1/2 lg:w-1/3">
+                    <div className="w-full p-4 md:w-1/2 lg:w-1/4">
                       <div className="flex flex-col h-full p-8 bg-gray-100 rounded-lg">
                         <div className="flex items-center mb-3">
                           <h2 className="text-2xl font-medium text-gray-900 title-font">
@@ -47,12 +47,12 @@ export default function Home() {
                           </h2>
                         </div>
                         <div className="flex-grow mt-2">
-                          <p className="inline-flex items-baseline">
-                            <span className="text-xl font-semibold">27</span>
+                          <p className="inline-flex items-end">
+                            <span className="text-2xl font-semibold">27</span>
                             <span className="ml-2">Posko</span>
                           </p>
-                          <p className="inline-flex items-baseline mt-2">
-                            <span className="text-xl font-semibold text-yellow-500">
+                          <p className="inline-flex items-end mt-2">
+                            <span className="text-2xl font-semibold text-yellow-500">
                               60%
                             </span>
                             <span className="ml-2">Bantuan terpenuhi</span>
@@ -60,7 +60,7 @@ export default function Home() {
                         </div>
                       </div>
                     </div>
-                    <div className="p-4 md:w-1/2 lg:w-1/3">
+                    <div className="w-full p-4 md:w-1/2 lg:w-1/4">
                       <div className="flex flex-col h-full p-8 bg-gray-100 rounded-lg">
                         <div className="flex items-center mb-3">
                           <h2 className="text-2xl font-medium text-gray-900 title-font">
@@ -68,12 +68,12 @@ export default function Home() {
                           </h2>
                         </div>
                         <div className="flex-grow mt-2">
-                          <p className="inline-flex items-baseline">
-                            <span className="text-xl font-semibold">27</span>
+                          <p className="inline-flex items-end">
+                            <span className="text-2xl font-semibold">27</span>
                             <span className="ml-2">Posko</span>
                           </p>
-                          <p className="inline-flex items-baseline mt-2">
-                            <span className="text-xl font-semibold text-green-500">
+                          <p className="inline-flex items-end mt-2">
+                            <span className="text-2xl font-semibold text-green-500">
                               90%
                             </span>
                             <span className="ml-2">Bantuan terpenuhi</span>
@@ -81,7 +81,7 @@ export default function Home() {
                         </div>
                       </div>
                     </div>
-                    <div className="p-4 md:w-1/2 lg:w-1/3">
+                    <div className="w-full p-4 md:w-1/2 lg:w-1/4">
                       <div className="flex flex-col h-full p-8 bg-gray-100 rounded-lg">
                         <div className="flex items-center mb-3">
                           <h2 className="text-2xl font-medium text-gray-900 title-font">
@@ -89,12 +89,12 @@ export default function Home() {
                           </h2>
                         </div>
                         <div className="flex-grow mt-2">
-                          <p className="inline-flex items-baseline">
-                            <span className="text-xl font-semibold">27</span>
+                          <p className="inline-flex items-end">
+                            <span className="text-2xl font-semibold">27</span>
                             <span className="ml-2">Posko</span>
                           </p>
-                          <p className="inline-flex items-baseline mt-2">
-                            <span className="text-xl font-semibold text-red-500">
+                          <p className="inline-flex items-end mt-2">
+                            <span className="text-2xl font-semibold text-red-500">
                               20%
                             </span>
                             <span className="ml-2">Bantuan terpenuhi</span>
@@ -102,7 +102,7 @@ export default function Home() {
                         </div>
                       </div>
                     </div>
-                    <div className="p-4 md:w-1/2 lg:w-1/3">
+                    <div className="w-full p-4 md:w-1/2 lg:w-1/4">
                       <div className="flex flex-col h-full p-8 bg-gray-100 rounded-lg">
                         <div className="flex items-center mb-3">
                           <h2 className="text-2xl font-medium text-gray-900 title-font">
@@ -110,12 +110,12 @@ export default function Home() {
                           </h2>
                         </div>
                         <div className="flex-grow mt-2">
-                          <p className="inline-flex items-baseline">
-                            <span className="text-xl font-semibold">27</span>
+                          <p className="inline-flex items-end">
+                            <span className="text-2xl font-semibold">27</span>
                             <span className="ml-2">Posko</span>
                           </p>
-                          <p className="inline-flex items-baseline mt-2">
-                            <span className="text-xl font-semibold text-yellow-500">
+                          <p className="inline-flex items-end mt-2">
+                            <span className="text-2xl font-semibold text-yellow-500">
                               60%
                             </span>
                             <span className="ml-2">Bantuan terpenuhi</span>
@@ -123,7 +123,7 @@ export default function Home() {
                         </div>
                       </div>
                     </div>
-                    <div className="p-4 md:w-1/2 lg:w-1/3">
+                    <div className="w-full p-4 md:w-1/2 lg:w-1/4">
                       <div className="flex flex-col h-full p-8 bg-gray-100 rounded-lg">
                         <div className="flex items-center mb-3">
                           <h2 className="text-2xl font-medium text-gray-900 title-font">
@@ -131,12 +131,12 @@ export default function Home() {
                           </h2>
                         </div>
                         <div className="flex-grow mt-2">
-                          <p className="inline-flex items-baseline">
-                            <span className="text-xl font-semibold">27</span>
+                          <p className="inline-flex items-end">
+                            <span className="text-2xl font-semibold">27</span>
                             <span className="ml-2">Posko</span>
                           </p>
-                          <p className="inline-flex items-baseline mt-2">
-                            <span className="text-xl font-semibold text-green-500">
+                          <p className="inline-flex items-end mt-2">
+                            <span className="text-2xl font-semibold text-green-500">
                               90%
                             </span>
                             <span className="ml-2">Bantuan terpenuhi</span>
@@ -144,7 +144,7 @@ export default function Home() {
                         </div>
                       </div>
                     </div>
-                    <div className="p-4 md:w-1/2 lg:w-1/3">
+                    <div className="w-full p-4 md:w-1/2 lg:w-1/4">
                       <div className="flex flex-col h-full p-8 bg-gray-100 rounded-lg">
                         <div className="flex items-center mb-3">
                           <h2 className="text-2xl font-medium text-gray-900 title-font">
@@ -152,12 +152,12 @@ export default function Home() {
                           </h2>
                         </div>
                         <div className="flex-grow mt-2">
-                          <p className="inline-flex items-baseline">
-                            <span className="text-xl font-semibold">27</span>
+                          <p className="inline-flex items-end">
+                            <span className="text-2xl font-semibold">27</span>
                             <span className="ml-2">Posko</span>
                           </p>
-                          <p className="inline-flex items-baseline mt-2">
-                            <span className="text-xl font-semibold text-red-500">
+                          <p className="inline-flex items-end mt-2">
+                            <span className="text-2xl font-semibold text-red-500">
                               20%
                             </span>
                             <span className="ml-2">Bantuan terpenuhi</span>
@@ -165,7 +165,7 @@ export default function Home() {
                         </div>
                       </div>
                     </div>
-                    <div className="p-4 md:w-1/2 lg:w-1/3">
+                    <div className="w-full p-4 md:w-1/2 lg:w-1/4">
                       <div className="flex flex-col h-full p-8 bg-gray-100 rounded-lg">
                         <div className="flex items-center mb-3">
                           <h2 className="text-2xl font-medium text-gray-900 title-font">
@@ -173,12 +173,12 @@ export default function Home() {
                           </h2>
                         </div>
                         <div className="flex-grow mt-2">
-                          <p className="inline-flex items-baseline">
-                            <span className="text-xl font-semibold">27</span>
+                          <p className="inline-flex items-end">
+                            <span className="text-2xl font-semibold">27</span>
                             <span className="ml-2">Posko</span>
                           </p>
-                          <p className="inline-flex items-baseline mt-2">
-                            <span className="text-xl font-semibold text-yellow-500">
+                          <p className="inline-flex items-end mt-2">
+                            <span className="text-2xl font-semibold text-yellow-500">
                               60%
                             </span>
                             <span className="ml-2">Bantuan terpenuhi</span>
@@ -186,7 +186,7 @@ export default function Home() {
                         </div>
                       </div>
                     </div>
-                    <div className="p-4 md:w-1/2 lg:w-1/3">
+                    <div className="w-full p-4 md:w-1/2 lg:w-1/4">
                       <div className="flex flex-col h-full p-8 bg-gray-100 rounded-lg">
                         <div className="flex items-center mb-3">
                           <h2 className="text-2xl font-medium text-gray-900 title-font">
@@ -194,12 +194,12 @@ export default function Home() {
                           </h2>
                         </div>
                         <div className="flex-grow mt-2">
-                          <p className="inline-flex items-baseline">
-                            <span className="text-xl font-semibold">27</span>
+                          <p className="inline-flex items-end">
+                            <span className="text-2xl font-semibold">27</span>
                             <span className="ml-2">Posko</span>
                           </p>
-                          <p className="inline-flex items-baseline mt-2">
-                            <span className="text-xl font-semibold text-green-500">
+                          <p className="inline-flex items-end mt-2">
+                            <span className="text-2xl font-semibold text-green-500">
                               90%
                             </span>
                             <span className="ml-2">Bantuan terpenuhi</span>
